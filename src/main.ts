8f767f3e664a28a417debfcdf79f999a3f903643
Jack0237 <99824627+jack0237@users.noreply.github.com>
2026-01-08 12:31:58 +0100

    Add Swagger API documentation and env example file
    Introduced Swagger integration in main.ts for API documentation and added @nestjs/swagger to dependencies. Created an env.example file to document required environment variables for the application.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+
+    .setTitle('C15 Tour Back API')
+    .setDescription('The C15 Tour Back API description')
+    .setVersion('1.0')
+    .addTag('C15 Tour Back')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+
+  console.log(`Application is running on: http://localhost:${port}`);
+  console.log(`Swagger s available on: http://localhost:${port}/api`);
 }
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
